7fc7d24cfac0893f4666fc21f286168d58655498
mariorui <mariorui@mariorui-m-44f5.lan>
2022-11-09 17:42:27 -0500

    With Furong updates

--- Azure/AKS/aks.tf
+++ Azure/AKS/aks.tf
@@ -5,6 +5,20 @@ resource "azurerm_resource_group" "default" {
   location = var.location.value
 }
 
+resource "azurerm_virtual_network" "default" {
+  name                = "${random_pet.prefix.id}-vnet"
+  location            = var.location.value
+  resource_group_name = azurerm_resource_group.default.name
+  address_space       = ["10.0.0.0/8"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  virtual_network_name = azurerm_virtual_network.default.name
+  resource_group_name  = azurerm_resource_group.default.name
+  address_prefixes     = ["10.1.0.0/16"]      
+}
+
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "${random_pet.prefix.id}-aks"
   location            = var.location.value
@@ -12,25 +26,17 @@ resource "azurerm_kubernetes_cluster" "default" {
   kubernetes_version = "1.22.15"
   dns_prefix          = "${random_pet.prefix.id}-k8s"
 
-  addon_profile {
-    kube_dashboard {
-      enabled = true
-    }
-  }
   default_node_pool {
     name            = "default"
     node_count      = 2
     vm_size         = "Standard_D2_v2"
     os_disk_size_gb = 30
+    vnet_subnet_id  = resource.azurerm_subnet.subnet.id
   }
 
   service_principal {
     client_id     = var.appId
     client_secret = var.password
-  }
-
-  role_based_access_control {
-    enabled = true
   }
 
    network_profile {
@@ -42,5 +48,6 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   tags = {
     Environment = "Prod"
+    owner       = "mariorui"
   }
 }
